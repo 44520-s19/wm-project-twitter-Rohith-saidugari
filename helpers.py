4b654186a93a7e64e42328befbf6a4a30e812884
Rohit Saidugari <S534735@nwmissouri.edu>
2019-06-18 21:42:18 -0500

    Commit helpers and notebook
    Commit

--- helpers.py
+++ helpers.py
@@ -7,11 +7,24 @@ Created on Mon Jun 17 23:45:33 2019
 
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
 def get_sentiments(text):
     analyzer = SentimentIntensityAnalyzer()
     return analyzer.polarity_scores(text)
+
+
 def split_sentiments(sentiments):
     xs = [sentiment['neg'] for sentiment in sentiments]
     ys = [sentiment['neu'] for sentiment in sentiments]
     zs = [sentiment['pos'] for sentiment in sentiments]
     return xs,ys,zs
+
+# lexical Diversity
+def lexical_diversity(text):
+    tokens = text.lower().split()
+    return len(set(tokens))/len(tokens)
+
+def average_words(statuses):
+    total_words = sum([ len(s.split()) for s in statuses ]) 
+    return total_words/len(statuses)
